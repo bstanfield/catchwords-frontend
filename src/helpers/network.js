f992ad1e364a4fd5493ba2be7788781a782856ad
Ben Stanfield <bnstnfld@gmail.com>
2020-08-31 21:28:58 -0700

    no localhost

--- src/helpers/network.js
+++ src/helpers/network.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://catchwords.herokuapp.com/api';
-const API_URL = 'http://localhost:3333/api';
+const API_URL = 'https://catchwords.herokuapp.com/api';
+// const API_URL = 'http://localhost:3333/api';
 
 function generateHeaders() {
   const headers = new Headers({
